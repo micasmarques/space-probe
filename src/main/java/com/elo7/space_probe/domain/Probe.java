9e553fe99f436c5190ef6f60dec3257b1eb4f99e
Micael Marques <micasmarques1132@gmail.com>
2024-10-14 15:25:34 -0300

    Adicionando movimentacao da sonda

--- src/main/java/com/elo7/space_probe/domain/Probe.java
+++ src/main/java/com/elo7/space_probe/domain/Probe.java
@@ -1,6 +1,15 @@
 package com.elo7.space_probe.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "probe")
@@ -46,5 +55,9 @@ public class Probe {
 
     public Integer getPlanetId() {
         return planet.getId();
+    }
+
+    public void updatePosition(int x, int y) {
+        this.position = new Position(x, y);
     }
 }
